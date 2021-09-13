8a9113f8d178bae936c6a501770f6e54a463cc75
rxOred <rxored@gmail.com>
2021-09-13 11:20:35 +0000

    globals

---
+++ parser/globals.go
@@ -0,0 +1,15 @@
+package parser
+
+var (
+	elf_type      string
+	elf_arch      string
+	elf_indianess string
+	elf_machine   string
+	elf_version   int
+	elf_entry     int
+	elf_phoff     int
+	elf_shoff     int
+	elf_phnum     int
+	elf_shnum     int
+	elf_flags     int
+)
