f23776ff2b8c5734c199d43504c6a33fa92bf8dc
rxOred <rxored@gmail.com>
2021-09-14 10:33:47 +0000

    elf parser

--- parser/parser.go
+++ parser/parser.go
@@ -2,33 +2,60 @@ package parser
 
 import (
 	"debug/elf"
+	"errors"
+	"io"
 	"os"
-
-	view "github.com/rxOred/unnatural/view"
 )
 
-func LoadElf(v *view.View, pathname string) *elf.File {
-	f, err := os.Open(pathname)
+var (
+	elf_machine string
+	elf_class   string
+)
+
+func ioReader(file string) (io.Reader, err) {
+	r, err := os.Open(file)
 	if err != nil {
-		v.ShowErrorMsg(err.Error())
+		return nil, err
 	}
-	defer f.Close()
-
-	elf, err := elf.NewFile(f)
-	if err != nil {
-		v.ShowErrorMsg(err.Error())
-	}
-
-	var ident [16]uint8
-	if _, err := f.ReadAt(ident[0:], 0); err != nil {
-		v.ShowErrorMsg("[!] Failed to read elf binary")
-	}
-
-	if ident[0] != '\x7f' || ident[1] != 'E' || ident[2] != 'L' || ident[3] != 'F' {
-		v.ShowErrorMsg("[!] File has a wrong magic number")
-	}
-
-	return elf
 }
 
-func ShowInfo()
+// i kinda half stole this function from golang source hehe :3
+func openElf(file string) (*elf.File, error) {
+	f, err := ioReader(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	ident := make([]uint8, 16)
+	if _, err = f.ReadAt(ident[0:], 0); err != nil {
+		return nil, err
+	}
+	if ident[0] != '\x7f' || ident[1] != 'E' || ident[2] != 'L' || ident[3] != 'F' {
+		return nil, errors.New("Invalid magic number")
+	}
+
+	ef := new(elf.File)
+	ef.Class = elf.Class(ident[elf.EI_CLASS])
+	switch ef.Class.String() {
+	case "ELFCLASS64":
+		elf_class = "64b"
+	case "ELFCLASS32":
+		elf_class = "32b"
+	}
+	ef.OSABI = elf.OSABI(ident[elf.EI_OSABI])
+	switch ef.OSABI.String() {
+	case "":
+		// ok
+	default:
+		return nil, errors.New("Invalid OSABI")
+	}
+
+	// parse elf header
+	var (
+		phoff, shoff         int64
+		phentsize, shentsize int
+		phnum, shnum         int
+		shstrndx             int // this is the thing that is most important for us to parse section names
+	)
+
+}
