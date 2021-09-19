f51f4c740a2954a6a6593ccd15fb5cb72781cc5c
rxOred <rxored@gmail.com>
2021-09-19 20:18:39 +0000

    events

--- view/events.go
+++ view/events.go
@@ -8,13 +8,19 @@ import (
 
 func (av *AnalysisView) Eventloop() {
 	for e := range ui.PollEvents() {
-		for e.Type == ui.KeyboardEvent {
+		if e.Type == ui.KeyboardEvent {
 			switch e.ID {
 			case "<C-c>":
 				os.Exit(1)
 			case "<C-s>":
-				av.Guagebar.Title = "Analysing"
-				// start analysis
+				_ = av.StartAnalysis()
+			case "<Down>":
+			case "<Up>":
+			case "<Left>":
+			case "<Right>":
+			default:
+				av.a_report.Rows = append(av.a_report.Rows, "key:"+e.ID)
+				ui.Render(av.a_grid)
 			}
 		}
 	}
