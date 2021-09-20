d1709c69ddb74715f0878a2719e93f026ddc8be1
rxOred <rxored@gmail.com>
2021-09-20 23:56:32 +0000

    events

--- view/events.go
+++ view/events.go
@@ -2,22 +2,49 @@ package view
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
 	ui "github.com/gizak/termui/v3"
 )
 
-func (av *AnalysisView) Eventloop() {
+func (av *AnalysisView) Eventloop(ev *ErrorView) {
+	sig_term := make(chan os.Signal, 2)
+	signal.Notify(sig_term, os.Interrupt, syscall.SIGTERM)
+
+	choices := []string{
+		"Sections",
+		"Elf Header",
+		"Symbols",
+		"Analysis Report",
+	}
+	var hightlight int8 = 0
+
 	for e := range ui.PollEvents() {
 		if e.Type == ui.KeyboardEvent {
 			switch e.ID {
 			case "<C-c>":
 				os.Exit(1)
+
 			case "<C-s>":
-				_ = av.StartAnalysis()
-			case "<Down>":
-			case "<Up>":
-			case "<Left>":
-			case "<Right>":
+				if err := av.StartAnalysis(); err != nil {
+					ShowErrorView(ev, err.Error())
+				}
+
+			case "Up", "<Left>":
+				hightlight--
+				if hightlight == -1 {
+					highight == 4
+				}
+				hightlight(1)
+
+			case "<Down>", "<Right>":
+				hightlight++
+				if hightlight == 5 {
+					hightlight = 0
+				}
+				HighLight(hightlight)
+
 			default:
 				av.a_report.Rows = append(av.a_report.Rows, "key:"+e.ID)
 				ui.Render(av.a_grid)
