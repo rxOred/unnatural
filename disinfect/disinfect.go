4bda80b1efe69907c9b4248eed90f40c7e289b4b
rxOred <rxored@gmail.com>
2021-09-24 14:35:06 +0000

    ..

--- disinfect/disinfect.go
+++ disinfect/disinfect.go
@@ -2,25 +2,29 @@ package disinfect
 
 import (
 	"debug/elf"
-	"errors"
+	"strconv"
 )
 
-func DisinfectTextPaddingInfection(f *elf.File) error {
+func DisinfectTextPaddingInfection(f *elf.File) []string {
 	sec := f.Section(".init")
 	for i := 0; i < len(f.Progs); i++ {
 		if f.Progs[i].Type == elf.PT_LOAD && f.Progs[i].Flags == elf.PF_R|elf.PF_X {
 			if sec.Addr >= f.Progs[i].Vaddr && sec.Addr < f.Progs[i].Vaddr+f.Progs[i].Memsz {
 				f.Entry = sec.Addr
-				return nil
+				r := []string{
+					"[SUCESSFUL] Disinfected text padding infection",
+					"Fixed entry point :", strconv.FormatUint(uint64(f.Entry), 16),
+				}
+				return r
 			}
 		}
 	}
-	return errors.New("Disinfection Failed")
+	return nil
 }
 
-func DisinfectDataSegmentInfection(f *elf.File) error {
-	if err := DisinfectTextPaddingInfection(f); err != nil {
-		return err
+func DisinfectDataSegmentInfection(f *elf.File) []string {
+	if r := DisinfectTextPaddingInfection(f); r != nil {
+		r[0] = "[SUCESSFUL] Disinfected data segment infection"
 	}
 	return nil
 }
