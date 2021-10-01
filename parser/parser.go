470fc6404fbccc0fd73a5508c9f4ddc1cc2eaf0e
rxOred <rxored@gmail.com>
2021-10-01 15:39:17 +0000

    parser

--- parser/parser.go
+++ parser/parser.go
@@ -12,6 +12,21 @@ type ElfFile struct {
 	elfHeader Ehdr
 	phdr      []Phdr
 	shdr      []Shdr
+	symtab    []Sym
+	strtab    []byte
+}
+
+func (e *ElfFile) GetSectionIndexByName(name string) (int, error) {
+	if e.elfHeader.EShstrndx == 0 {
+		return 0, errors.New("section header string table is empty")
+	}
+	shstrtab := &e.memmap[e.elfHeader.EShstrndx]
+	for i := 0; i < len(shstrtab); i++ {
+		if shstrtab+i == name {
+			return i, nil
+		}
+	}
+	return 0, errors.New("Could not find the section")
 }
 
 func LoadElf(e *ElfFile, pathname string) error {
@@ -30,6 +45,15 @@ func LoadElf(e *ElfFile, pathname string) error {
 
 	e.phdr = &e.memmap[e.elfHeader.EPhoff]
 	e.shdr = &e.memmap[e.elfHeader.EShoff]
+	symtab_index, err := e.GetSectionIndexByName(".symtab")
+	if err == nil && symtab_index != 0 {
+		e.symtab = &e.memmap[e.shdr[symtab_index].ShOffset]
+	}
+
+	strtab_index, err := e.GetSectionIndexByName(".strtab")
+	if err == nil && strtab_index != 0 {
+		e.strtab = &e.memmap[e.shdr[strtab_index].ShOffset]
+	}
 
 	return nil
 }
